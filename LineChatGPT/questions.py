b4c0a62b1feae83681a08157a256cc84e65af723
Poriteslutea <rubymagoo@gmail.com>
2023-02-16 21:56:39 +0800

    利用正規表達式清理爬蟲資料

--- LineChatGPT/questions.py
+++ LineChatGPT/questions.py
@@ -2,47 +2,42 @@ import requests
 from bs4 import BeautifulSoup
 from random import randint
 
+from collections import defaultdict
+import re
+
+
+
 # 取得網頁
 response = requests.get('https://www.tenlong.com.tw/products/9787111648178')
 soup = BeautifulSoup(response.text, "html.parser")
 
 
 
-# Python程序員面試筆試寶典 - 先擷取第二章內容
+# Python程序員面試筆試寶典 - 先擷取目錄內容
 table_contents = soup.find_all("div", class_="item-desc")[2]
-ch2 = table_contents.findAll('p')[1]
-ch2_qlist = ch2.getText().split('\r\n')
 
-q_2_6 = []
-q_2_7 = []
-q_2_8 = []
-q_2_9 = []
-for q in ch2_qlist:
-    if q.startswith('2.6.') and '？' in q:
-        qq = ''.join(q.split('？')[:-1] + ['?'])
-        qq = qq.split(' ')[1]
-        q_2_6.append(qq)
-    elif q.startswith('2.7.'):
-        qq = ''.join([i for i in q if not i.isdigit()])
-        qq = qq.split(' ')[1]
-        q_2_7.append(qq)
-    elif q.startswith('2.8.'):
-        qq = ''.join([i for i in q if not i.isdigit()])
-        qq = qq.split(' ')[1]
-        q_2_8.append(qq)
-    elif q.startswith('2.9.'):
-        qq = ''.join([i for i in q if not i.isdigit()])
-        qq = qq.split(' ')[1]
-        q_2_9.append(qq)
-    elif q.startswith('2.9.'):
-        qq = ''.join([i for i in q if not i.isdigit()])
-        qq = qq.split(' ')[1]
-        q_2_9.append(qq)       
+# chapter name list
+chapter = ['Python基礎題','Python進階題', '資料結構與演算法','資料庫相關','爬蟲','數據分析','Pandas','機器學習']
 
+# Create dict with chapter title as key name, sub title as value
+q_dict = defaultdict(list)
+for i in range(1,9):
+    ch = table_contents.findAll('p')[i]
+    ch_qlist = ch.getText().split('\r\n')
+    q_dict[chapter[i-1]] = ch_qlist
 
-ch2_dict = {'資料類型':q_2_6, '日期與時間':q_2_7, '流程控制語句':q_2_8, 'collection模組':q_2_9}
-ch2_ques = q_2_6 + q_2_7 + q_2_8 + q_2_9
-q_dict={'Python基礎題':ch2_ques}
+# 正規表達式
+role = re.compile(r'\d.\d.\d ')
+
+# 處理與選擇資料
+for k,v in q_dict.items():
+    new_ques = []
+    for ques in v:
+        if role.match(ques):
+            ques = ques.split(' ')[1]
+            ques = re.sub(r'\d+', '', ques)
+            new_ques.append(ques)
+    q_dict[k] = new_ques
 
 def get_question(catagory, q_dict=q_dict):
     q_list = q_dict[catagory]
